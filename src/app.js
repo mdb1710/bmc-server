2c2f9f17ba46a25712a448dfe5dc34bf949947a5
mdb1710 <mdb1710@gmail.com>
2020-01-28 13:49:25 -0500

    changed date to query

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 
+const { getChart } = require("billboard-top-100/billboard-top-100");
+
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -14,6 +16,42 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+
+let newCharts;
+
+function handleBillboardCharts(req, res) {
+  const { date } = req.query;
+
+  let chartType = "hot-100";
+  let chartDate = date;
+
+  getChart(chartType, chartDate, (err, chart) => {
+    if (err) {
+      res.send(err);
+    } else {
+      newCharts = chart.songs.map(entry => {
+        return {
+          id: entry.rank,
+          title: entry.title,
+          artist: entry.artist,
+          cover: entry.cover
+        };
+      });
+      console.log("The top song is SWV watch", newCharts[0]);
+    }
+  });
+
+  res
+    .status(200)
+    .json(newCharts)
+    .end();
+}
+
+// function getChart("hot-100", "1993-07-10", (err, chart) => {
+//   if (err) console.log(err);
+//   newCharts = chart.songs;
+//   console.log("The top song is SWV watch:", newCharts[0]);
+// });
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
@@ -29,5 +67,7 @@ app.use(function errorHandler(error, req, res, next) {
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.get("/charts", handleBillboardCharts);
 
 module.exports = app;
