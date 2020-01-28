1ad40c9d22ee51511d7739e1e88689b09069480c
mdb1710 <mdb1710@gmail.com>
2020-01-28 11:27:45 -0500

    temp change of PORT

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-const app = require('./app')
+"use strict";
 
-const PORT = require('.config')
+const app = require("./app");
+
+// const PORT = require("./config");
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
